3c57e5b72bfbddad86bcbed1b2ab90892beaa39e
anthony kiarie <akiarie27@gmail.com>
2025-03-18 06:27:28 +0300

    the 1st  index.js commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+const welcomeMessage= `'Welcome to Flatbook, ' ${currentUser}!`;
+
+//welcomeMessage = `welcomeMessage, ${welcomeMessage}!`;
+const excitedWelcomeMessage =welcomeMessage.toUpperCase();
+
+const shortGreeting  =`Welcome, ${currentUser.slice(0,1)}!`;
